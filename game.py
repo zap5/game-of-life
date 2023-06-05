27e966a28e52f82892106ac6bb595e1924e05e3a
Zach Pierce <19274781+zap5@users.noreply.github.com>
2023-06-05 14:26:37 -0700

    Wow this code is so ugly but it WORKS

--- game.py
+++ game.py
@@ -53,51 +53,77 @@ class Life():
     
     def check_neighbors(self, x, y):
         living_neighbor_count = 0
-        current_row_index = x 
-        row_above_index = current_row_index - 1
-        row_below_index = current_row_index + 1
+        row_index = x 
 
         cell_index = y
-        right_cell_index = y + 1
-        left_cell_index = y - 1
 
-        try:
-            for i in range(row_above_index, row_below_index):
-                for j in range(left_cell_index, right_cell_index):
-                    if self.board[i][j] == 1:
-                        living_neighbor_count += 1
-        except IndexError:
-            pass
+        for i in range(row_index - 1, row_index + 2):
+            if i < 0 or i >= self.height:
+                continue
+            # print("Checking row ", str(i))
+            for j in range(cell_index - 1, cell_index + 2):
+                if j < 0 or j >= self.width:
+                    continue
+                if i == x and j == y:
+                    continue
+                # print("Checking neighbors in row ", str(i),  ", cell ", str(j))
+                if self.board[i][j] == 1:
+                    living_neighbor_count += 1
+                    # print("Living neighbor count is now ", str(living_neighbor_count))
         return living_neighbor_count
     
-    def next_board_state(self):
+    def copy_board(self):
+        new_board = [[]] * self.height
+        for i in range(self.height):
+            new_board[i] = [0] * self.width 
+
+        for i in range(self.height):
+            for j in range(self.width):
+                new_board[i][j] = self.board[i][j]
+        
+        return new_board
+
+    def determine_next_board_state(self):
         # Any cell with 0 to 1 live neighbors becomes dead, because of underpopulation
         # Any cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
         # Any cell with more than 3 live neighbors dies, due to overpopulation
         # Any dead cell with exactly 3 neighbors becomes alive, due to reproduction
-
-        new_board = self.board
+        
+        new_state_board = self.copy_board()
         
         for i in range(self.height):
             for j in range(self.width):
+                # print("Checking neighbors of cell ", str(i), ", ", str(j))
                 live_ones = self.check_neighbors(i, j)
+                # print("Live ones count: ", str(live_ones))
+                # print("Value of cell ", str(i), ", ", str(j), ": ", str(self.board[i][j]))
                 if live_ones <= 1:
-                    new_board[i][j] = 0
+                    new_state_board[i][j] = 0
                 elif live_ones == 2:
-                    pass
+                    continue
                 elif live_ones == 3: 
-                    new_board[i][j] = 1
+                    new_state_board[i][j] = 1
                 elif live_ones > 3:
-                    new_board[i][j] = 0
+                    new_state_board[i][j] = 0
+                
+                # print("Value of cell ", str(i), ", ", str(j), ": ", str(self.board[i][j]))
         
-        self.board = new_board
+        return new_state_board
+    
+    def set_next_board_state(self):
+        next_board_state = self.determine_next_board_state()
+
+        for i in range(self.height):
+            for j in range(self.width):
+                self.board[i][j] = next_board_state[i][j]
 
 
 x = Life()
-x.set_board_size(10, 20)
+x.set_board_size(100, 100)
 x.random_state()
+# x.board = [[1, 0, 1], [0, 0, 0], [1, 0, 0]]
 
 while True:
+    x.set_next_board_state()
     x.render_board()
-    x.next_board_state()
     sleep(1)
